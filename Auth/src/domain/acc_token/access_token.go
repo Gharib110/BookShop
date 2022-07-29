672a46967796220b574cec8a7d11d581d20f757b
AlirezaGharib <alirezagharib110@gmail.com>
2022-07-29 18:48:31 +0430

    Auth API was initialized

---
+++ Auth/src/domain/acc_token/access_token.go
@@ -0,0 +1,18 @@
+package acc_token
+
+import "time"
+
+const expirationTime = 24
+
+type AccessToken struct {
+	AccessTk string `json:"access_token"`
+	UserID   int64  `json:"user_id"`
+	ClientID int64  `json:"client_id"` // Web Front-End Client-ID
+	Expires  int64  `json:"expires"`
+}
+
+func GetNewAccessToken() AccessToken {
+	return AccessToken{
+		Expires: time.Now().UTC().Add(expirationTime * time.Minute).Unix(),
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BookShop
 A BookShop, will be designed as a microservices
+- Start the Auth Server
+- Start BookShop (Core of the App)
+- Start Item Vendors Server
